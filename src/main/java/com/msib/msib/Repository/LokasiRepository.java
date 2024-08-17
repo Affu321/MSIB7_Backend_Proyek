d1e7afe4107c9a40ed9ec5df2a016c5f50ea00da
Affu321 <affudina663@gmail.com>
2024-08-17 17:10:53 +0800

    add Repository

---
+++ src/main/java/com/msib/msib/Repository/LokasiRepository.java
@@ -0,0 +1,10 @@
+package com.msib.msib.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LokasiRepository extends JpaRepository<LokasiRepository, Long>, JpaSpecificationExecutor<LokasiRepository> {
+
+}
